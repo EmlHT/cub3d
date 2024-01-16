feb410d49808abfa14d050e0bee5c4e903b636ad
brettleclerc13 <brettleclerc13@gmail.com>
2024-01-16 15:11:23 +0100

    initiated movements

--- src/raycasting/movements.c
+++ src/raycasting/movements.c
@@ -6,7 +6,7 @@
 /*   By: brettleclerc <brettleclerc@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:01:11 by brettlecler       #+#    #+#             */
-/*   Updated: 2024/01/16 15:08:29 by brettlecler      ###   ########.fr       */
+/*   Updated: 2024/01/16 15:11:01 by brettlecler      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	ft_movement_a(t_cub *cube)
 
 void	ft_movement_w(t_cub *cube)
 {
-	if (cube->map[cube->mlx.pos.y - 1][])
+	if (cube->map[cube->mlx.pos.y - 1][] == '1')
+		break ;
+	else
+		
 }
 
 void	ft_movement_d(t_cub *cube)
