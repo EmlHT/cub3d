727c83451281f2e279ed2842d462852b67ec6688
brettleclerc13 <brettleclerc13@gmail.com>
2024-01-19 08:50:00 +0100

    added floor and ceiling as per colours indicated in .cub

--- src/raycasting/textures.c
+++ src/raycasting/textures.c
@@ -6,7 +6,7 @@
 /*   By: brettleclerc <brettleclerc@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 17:03:49 by brettlecler       #+#    #+#             */
-/*   Updated: 2024/01/18 18:50:20 by brettlecler      ###   ########.fr       */
+/*   Updated: 2024/01/19 08:47:11 by brettlecler      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,42 @@
 
 int	rgb_to_hex(int red, int green, int blue)
 {
-	int hex_value;
+	int	hex_value;
 
 	hex_value = (red << 16) | (green << 8) | blue;
 	return (hex_value);
 }
 
-int	fill_colors(t_cub *cube, char type)
-{
-	char	**dest;
-	int		res;
-	
-	dest = NULL;
-	if (type == 'c')
-		dest = ft_split(cube->c, ',');
-	else if (type == 'f')
-		dest = ft_split(cube->f, ',');
-	res = rgb_to_hex(ft_atoi(dest[0]), ft_atoi(dest[1]), ft_atoi(dest[2]));
-	return (res);
-}
-
 void	color_ceiling_floor(t_cub *cube)
 {
 	int	i;
-    int	j;
+	int	j;
 
-    i = -1;
+	i = -1;
 	while (++i < SCREEN_HEIGHT)
-    {
-        j = -1;
-        while (++j < SCREEN_WIDTH)
+	{
+		j = -1;
+		while (++j < SCREEN_WIDTH)
 		{
 			if (i < SCREEN_HEIGHT / 2)
-				my_mlx_pixel_put(cube->mlx.img, j, i, fill_colors(cube, 'c'));
+			{
+				my_mlx_pixel_put(cube->mlx.img, j, i, \
+				rgb_to_hex(cube->c_colours->r, cube->c_colours->g, \
+				cube->c_colours->b));
+			}
 			else
-				my_mlx_pixel_put(cube->mlx.img, j, i, fill_colors(cube, 'f'));
+			{
+				my_mlx_pixel_put(cube->mlx.img, j, i, \
+				rgb_to_hex(cube->f_colours->r, cube->f_colours->g, \
+				cube->f_colours->b));
+			}
 		}
-    }
+	}
 }
 
 static unsigned int	get_pixel_color(t_img image, int x, int y)
 {
-	char    *color;
+	char	*color;
 
 	color = image.addr + (y * image.line_len + x * (image.bpp / 8));
 	return (*(unsigned int *)color);
@@ -71,7 +65,8 @@ void	draw_texture(t_cub *cube, int x)
 	{
 		cube->mlx.tex.y = (int)cube->mlx.tex.pos & TEXTURE_HEIGHT - 1;
 		cube->mlx.tex.pos += cube->mlx.tex.step;
-		color = get_pixel_color(cube->mlx.xpm_img.no, cube->mlx.tex.x, cube->mlx.tex.y);
+		color = get_pixel_color(cube->mlx.xpm_img.no, cube->mlx.tex.x, \
+		cube->mlx.tex.y);
 		my_mlx_pixel_put(cube->mlx.img, x, y, color);
 		y++;
 	}
